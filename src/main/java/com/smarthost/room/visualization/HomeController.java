7313165bc6aee2279bb8ca4f4ed9734b0ed9cfdf
Igor Silva <igortkd2006@gmail.com>
2021-02-20 19:59:23 -0300

    feat: add swagger documentation

--- src/main/java/com/smarthost/room/visualization/HomeController.java
+++ src/main/java/com/smarthost/room/visualization/HomeController.java
@@ -1,17 +1,20 @@
 package com.smarthost.room.visualization;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public Map<String, Object> greeting() {
-        return Collections.singletonMap("message", "Hello, World");
+    void hello(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
     }
 
 }
